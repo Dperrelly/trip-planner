da0545a7ac14c44a1d435a8f6de93879144807bd
Daniel Perrelly <danperrelly@gmail.com>
2015-08-19 12:52:08 -0400

    tried to seed

---
+++ models/index.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dropDatabase');
+mongoose.connection.on('error', console.error.bind(console, 'MongoDb connection error: '));
+
+var placeSchema = new mongoose.Schema({
+	address: {type: String},
+	city: {type: String},
+	state: {type: String},
+	phone: {type: String},
+	location: {type:[Number]}
+});
+
+var hotelSchema = new mongoose.Schema({
+	name: {type: String, required: true},
+	place: {type: mongoose.Schema.Types.ObjectId, ref: 'Place'},
+	num_stars: {type: Number, min: 1, max: 5},
+	amenities: {type: String}
+});
+
+var activitySchema = new mongoose.Schema({
+	name: {type: String, required: true},
+	place: {type: mongoose.Schema.Types.ObjectId, ref: 'Place'},
+	age_range: {type: String}
+});
+
+var restaurantSchema = new mongoose.Schema({
+	name: {type: String, required: true},
+	place: {type: mongoose.Schema.Types.ObjectId, ref: 'Place'},
+	cuisines: {type: String},
+	price: {type: Number, min: 1, max: 5}
+});
+
+var Place = mongoose.model('Place', placeSchema);
+var Hotel = mongoose.model('Hotel', hotelSchema);
+var Activity = mongoose.model('Activity', activitySchema);
+var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+module.exports = {
+	Place: Place,
+	Hotel: Hotel,
+	Activity: Activity,
+	Restaurant: Restaurant
+};
